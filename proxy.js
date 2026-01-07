fee819f34d42cd612d0ef8338a5647a7158e6e95
shabihabbas09 <shabihabbas09@gmail.com>
2026-01-07 08:07:32 +0000

    middleware to proxy

--- proxy.js
+++ proxy.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { getUser } from "./src/utils/dal";
-export async function middleware(request){
+export async function proxy(request){
     const user = await getUser();
     if(!user)
         return NextResponse.redirect(new URL("/login", request.url))
